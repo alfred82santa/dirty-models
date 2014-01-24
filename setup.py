56138d351de7329830fed8ad13af4a4e69f451be
Toni Marqués <amm@tid.es>
2014-01-24 08:58:28 +0100

    Setup.py added with version 0.1

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dirty-models',
+    url='https://github.com/alfred82santa/dirty-models',
+    author='alfred82santa',
+    version='0.1',
+    author_email='alfred82santa@gmail.com',
+    packages = find_packages(exclude=['test*']),
+    include_package_data=True,
+    test_suite="nose.collector",
+    tests_require="nose",
+    zip_safe=False,
+)
