969465791d6abf29badd1ea4bdbf4dfd25104134
Alfred Santacatalina <alfred82santa@gmail.com>
2014-09-16 00:12:18 +0200

    FastDynamicModel & fixes

--- performancerunner.py
+++ performancerunner.py
@@ -6,13 +6,17 @@ Created on 15/09/2014
 from performance import Runner
 from performance.dynamicmodel import DynamicModelPerformance
 from performance.blobfield import BlobFieldPerformance
+from performance.fastdynamicmodel import FastDynamicModelPerformance
 
 config = {'DynamicModel': {'test_class': DynamicModelPerformance,
                            'repeats': 5,
                            'params': {'depth': 6, 'children_count': 6}},
           'BlobField': {'test_class': BlobFieldPerformance,
-                           'repeats': 5,
-                           'params': {'depth': 6, 'children_count': 6}}}
+                        'repeats': 5,
+                        'params': {'depth': 6, 'children_count': 6}},
+          'FastDynamicModel': {'test_class': FastDynamicModelPerformance,
+                               'repeats': 5,
+                               'params': {'depth': 6, 'children_count': 6}}}
 
 if __name__ == '__main__':
 
